8b7b346fd5a812d7500c2f0efd320088dbce18e2
deagustinchristian <deagustinchristian@gmail.com>
2022-01-03 19:57:46 +0000

    Add JS functions so score updates and game logics

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,46 @@
 const buttons = document.querySelectorAll('.game-btn');
-
 const choices = ["rock", "paper", "scissors"];
+const scoreEl = document.getElementById('score');
 
+
+let score = 0;
 let userChoice = undefined;
 
 /**
- * below function works, checked in dev tools, when i press on one button it shows
+ * below function works, checked in dev tools, when i press on one button it logs
  */
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         userChoice = button.getAttribute('data-choice');
+
+        checkWinner();
     });
 });
 
+function checkWinner() {
+    const computerChoice = pickRandomChoice();
+
+    if (userChoice === computerChoice) {
+        // draw
+    } else if (
+        (userChoice === 'paper' && computerChoice === 'rock')
+        ||
+        (userChoice === 'rock' && computerChoice === 'scissors') 
+        ||
+        (userChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        //user wins
+        updateScore(1);
+    } else {
+        //user lost
+        updateScore(-1);
+    }
+}
+
+function updateScore(value) {
+    score += value;
+    scoreEl.innerText = score;
+}
 /**
  * below function works, tried in dev tools and computer choices are working
  */
