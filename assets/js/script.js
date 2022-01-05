8d16cf0d5b6d00bfcead5824865a29c3131a83f6
deagustinchristian <deagustinchristian@gmail.com>
2022-01-05 14:50:02 +0000

    Add game logic, rock beats scissors and so on

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,41 @@ options.forEach((option) => {
         const computerInput = computerOptions[Math.floor(Math.random() * 3)];
 
         compareInputs(playerInput, computerInput);
-        
     });
 });
+
+function compareInputs(playerInput, computerInput) {
+    const currentGame = `${playerInput} vs ${computerInput}`;
+
+    /**
+     * This checks if its a tie
+     */
+    if (playerInput === computerInput) {
+        alert(`${currentGame} is a Tie`);
+        return;
+    }
+
+    if (playerInput === "Rock") {
+        if (computerInput === "Scissors") {
+            alert(`${currentGame} = You Win!`);
+        } else {
+            alert(`${currentGame} = Computer Wins!`);
+        }
+    }
+
+    else if (playerInput === "Paper") {
+        if (computerInput === "Rock") {
+            alert(`${currentGame} = You Win!`);
+        } else {
+            alert(`${currentGame} = Computer Wins!`);
+        }
+    }
+
+    else if (playerInput === "Scissors") {
+        if (computerInput === "Paper") {
+            alert(`${currentGame} = You Win!`);
+        } else {
+            alert(`${currentGame} = Computer Wins!`)
+        }
+    }
+}
